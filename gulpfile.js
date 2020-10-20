cd960e4380fc0f41d6c9414fcd964aa7484575e2
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-10-20 16:16:32 -0500

    removed comments from gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
-// NOTE: Install all the necessary dependencies
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-// NOTE: This sass.compiler allows us to chose node-sass or dart-sass (sass)
-//NOTE: Je vais utiliser dart-sass puisque node-sass sera deprecated
 sass.compiler = require("sass");
 const htmlMin = require("gulp-htmlmin");
 const cleanCSS = require("gulp-clean-css");
 const browserSync = require("browser-sync").create();
-/**
- * NOTE: Transpiles our ES6 to ES5
- * @babel/core AND @babel/preset-env are needed for full transpilation to work
- */
 const babel = require("gulp-babel");
-//NOTE: Minifies our JS
 const uglify = require("gulp-uglify");
 
 function minifyHTML() {
@@ -28,46 +20,24 @@ function minifyHTML() {
 }
 
 function style() {
-  //TODO: Find my SCSS file in src folder
-  return (
-    gulp
-      .src("./src/sass/**/*.scss")
-      //TODO: Pass that file through Sass compiler - our gulp-sass package under the
-      // name sass handles this.
-      //TODO: Addition modify for better error output
-      .pipe(sass().on("error", sass.logError))
-      //TODO: Minify the newly transformed CSS
-      .pipe(cleanCSS())
-      //TODO: Output the transformed SCSS-come-minified CSS to our inside a CSS foler inside our dist folder
-      .pipe(gulp.dest("./dist/css"))
-      //QUESTION: What does this do again? watch Kevin Powell's gulp video to refresh
-      .pipe(browserSync.stream())
-  );
+  return gulp
+    .src("./src/sass/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream());
 }
 
-/**
- * TODO: Transpile our ES6 javascript to ES5. This is important
- * TODO: Minify our newly transpiled ES5 JS.
- */
 function configureJS() {
-  //TODO: Target desired file(s). Dans notre cas, c'est our index.js dedans le dossier src
-  return (
-    gulp
-      .src("./src/js/*")
-      /**
-       * NOTE: This code below transpiles our ES6 to ES5.
-       * IMPORTANT:NOTE: @babel/core and @babel/preset-env need to be installed along babel
-       * don't forget the "gulp-" prefix
-       */
-      .pipe(
-        babel({
-          //NOTE: @babel/preset-env package makes this line work
-          presets: ["@babel/preset-env"],
-        })
-      )
-      .pipe(uglify())
-      .pipe(gulp.dest("./dist/js"))
-  );
+  return gulp
+    .src("./src/js/*")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist/js"));
 }
 
 function watch() {
