e7358058a3ab00c17e67d8339fb1a9a97cde6b4f
Gilbert Rabut Tsurwa <tsurwagilbert@gmail.com>
2021-11-15 15:19:52 -0500

    Merge v2-no-comments code to this branch - there were no conflict issues for this one

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Gulp Front-End Workflow (v2)
+# Gulp Front-End Workflow (v1)
 
-If you want to see the specific notes on this version of the template, cliquer <a href="https://github.com/GillyRabutTsurwa/gulp-workflow-template/tree/v2#gulp-front-end-workflow-v2" target="_blank">ici</a>
+If you want to see the specific notes on this version of the template, cliquer <a href="https://github.com/GillyRabutTsurwa/gulp-workflow-template/tree/v2#gulp-front-end-workflow-v1" target="_blank">ici</a>
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
+// NOTE: Install all the necessary dependencies
 const gulp = require("gulp");
+// NOTE: the call back qui fait partie de la deuxieme partie du code allows us to chose node-sass or dart-sass (sass)
+//NOTE: Je vais utiliser dart-sass puisque node-sass sera deprecated
 const sass = require("gulp-sass")(require("sass"));
+sass.compiler = require("sass");
 const htmlMin = require("gulp-htmlmin");
 const cleanCSS = require("gulp-clean-css");
 const browserSync = require("browser-sync").create();
+/**
+ * NOTE: Transpiles our ES6 to ES5
+ * @babel/core AND @babel/preset-env are needed for full transpilation to work
+ */
 const babel = require("gulp-babel");
+//NOTE: Minifies our JS
 const uglify = require("gulp-uglify");
 
 function minifyHTML() {
@@ -19,24 +28,46 @@ function minifyHTML() {
 }
 
 function style() {
-  return gulp
-    .src("./src/sass/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("./dist/css"))
-    .pipe(browserSync.stream());
+  //TODO: Find my SCSS file in src folder
+  return (
+    gulp
+      .src("./src/sass/**/*.scss")
+      //TODO: Pass that file through Sass compiler - our gulp-sass package under the
+      // name sass handles this.
+      //TODO: Addition modify for better error output
+      .pipe(sass().on("error", sass.logError))
+      //TODO: Minify the newly transformed CSS
+      .pipe(cleanCSS())
+      //TODO: Output the transformed SCSS-come-minified CSS to our inside a CSS foler inside our dist folder
+      .pipe(gulp.dest("./dist/css"))
+      //QUESTION: What does this do again? watch Kevin Powell's gulp video to refresh
+      .pipe(browserSync.stream())
+  );
 }
 
+/**
+ * TODO: Transpile our ES6 javascript to ES5. This is important
+ * TODO: Minify our newly transpiled ES5 JS.
+ */
 function configureJS() {
-  return gulp
-    .src("./src/js/*")
-    .pipe(
-      babel({
-        presets: ["@babel/preset-env"],
-      })
-    )
-    .pipe(uglify())
-    .pipe(gulp.dest("./dist/js"));
+  //TODO: Target desired file(s). Dans notre cas, c'est our index.js dedans le dossier src
+  return (
+    gulp
+      .src("./src/js/*")
+      /**
+       * NOTE: This code below transpiles our ES6 to ES5.
+       * IMPORTANT:NOTE: @babel/core and @babel/preset-env need to be installed along babel
+       * don't forget the "gulp-" prefix
+       */
+      .pipe(
+        babel({
+          //NOTE: @babel/preset-env package makes this line work
+          presets: ["@babel/preset-env"],
+        })
+      )
+      .pipe(uglify())
+      .pipe(gulp.dest("./dist/js"))
+  );
 }
 
 function watch() {
@@ -47,14 +78,23 @@ function watch() {
     browser: "firefox",
   });
 
-  gulp.watch(["./src/*.html", "./src/sass/**/*.scss", "./src/js/app.js"], gulp.parallel(minifyHTML, style, configureJS));
+  // specific files to watch for with their respective changes (as functions) upon changes
+  gulp.watch("./src/*.html", minifyHTML);
+  gulp.watch("./src/sass/**/*.scss", style);
+  gulp.watch("./src/js/app.js", configureJS);
 
+  // These two files will refresh the page upon any changes
   gulp.watch("./src/*.html").on("change", browserSync.reload);
   gulp.watch("./src/js/app.js").on("change", browserSync.reload);
 }
 
+// NEWIMPORTANT: default task. what is what runs when we just type "gulp" in the command line.
+//NOTE:
 exports.default = gulp.series(gulp.parallel(minifyHTML, style, configureJS), watch);
 
+// These can stay in case we want to run our tasks individually.
+// these tasks if needed to be run are run by typing gulp [taskName]
+// like gulp minifyHTML or gulp style etc.
 exports.minifyHTML = minifyHTML;
 exports.style = style;
 exports.configureJS = configureJS;
